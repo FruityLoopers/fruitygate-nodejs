78ca9e35b0a8205ff389149496da6df32f3b71bd
Pete Hodgson <git@thepete.net>
2015-10-27 21:29:20 -0700

    minor cleanup

--- client/main.js
+++ client/main.js
@@ -8,15 +8,15 @@ function objValues(obj){
   return Object.keys(obj).map(key => obj[key]);
 }
 
-
-function render(nodes){
+function render(meshStatus){
+  const nodes = objValues(meshStatus.nodes);
   ReactDOM.render(<Main nodes={nodes}/>, document.getElementsByTagName('main')[0]);
 }
 
 function fetchMeshStatusAndRender(){
   fetch('/mesh-status')
   .then( (response)=> response.json() )
-  .then( (meshStatus)=> render(objValues(meshStatus.nodes)) );
+  .then( render );
 }
 
 setInterval( fetchMeshStatusAndRender, 1000 );
